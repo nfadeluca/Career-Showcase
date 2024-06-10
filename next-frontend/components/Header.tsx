789d6e85818022df5218c5bfe7846e4a6492c625
ndeluca <ndeluca@gmu.edu>
2024-06-10 18:52:42 -0400

    resize header text

--- next-frontend/components/Header.tsx
+++ next-frontend/components/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
           <div className="flex items-center justify-between">
             <div className="mr-3">{/* <Logo /> */}</div>
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
+              <div className="hidden h-8 text-4xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
@@ -22,14 +22,14 @@ const Header = () => {
           </div>
         </Link>
       </div>
-      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+      <div className="flex items-center space-x-8 leading-5 sm:space-x-10">
         {headerNavLinks
           .filter((link) => link.href !== '/')
           .map((link, index, arr) => (
             <Link
               key={link.title}
               href={link.href}
-              className={`hidden font-medium sm:block transform transition duration-200 hover:scale-105 hover:text-primary-500 dark:hover:text-primary-500 active:scale-95 active:text-primary-700 ${index === arr.length - 1 ? 'text-primary-600 animate-pulsate' : 'text-gray-900 dark:text-gray-100'
+              className={`hidden text-xl font-medium sm:block transform transition duration-200 hover:scale-105 hover:text-primary-500 dark:hover:text-primary-500 active:scale-95 active:text-primary-700 ${index === arr.length - 1 ? 'text-primary-600 animate-pulsate' : 'text-gray-900 dark:text-gray-100'
                 }`}
             >
               {link.title}
